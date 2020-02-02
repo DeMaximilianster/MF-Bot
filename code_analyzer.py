56470323b29f5a7f37874bf4fb14226bbf7630fc
sliva0 <sliva0mk@gmail.com>
2020-02-02 22:00:57 +0200

    Update code_analyzer.py
    code_analyzer update

--- code_analyzer.py
+++ code_analyzer.py
@@ -6,25 +6,15 @@ def write_in_bufer(text):
     root.clipboard_clear()
     root.clipboard_append(text)
 
-
 def open_button(text):
-    func = lambda:os.system(f'"C:\Windows\explorer.exe" {text}')
-    return Button(text='Open', command=func, bg='#503872', fg=fg)
+    l = Label(text='[Open file]', **bdict)
+    l.bind('<Button-1>',lambda x:os.system(f'"C:\Windows\explorer.exe" {text}'))
+    return l
 
 def copy_button(btext ,text):
-    func = lambda:write_in_bufer(text)
-    return Button(text=btext, command=func, bg='#503872', fg=fg)
-
-def show_path_button(text):
-    def func(e):
-        if e.widget['text'] == 'Show path':
-            e.widget['text'] = text
-        else:
-            e.widget['text'] = 'Show path'
-    b = Button(text='Show path', bg='#503872', fg=fg)
-    b.bind('<Button-1>', func)
-    return b
-
+    l = Label(text=f'[{btext}]', **bdict)
+    l.bind('<Button-1>', lambda x:write_in_bufer(text))
+    return l
 
 def smth_in(target, *args):
     for arg in args:
@@ -32,29 +22,45 @@ def smth_in(target, *args):
             return True
     return False
 
+def find_string_gen(search_text):
+    for path in files_list:
+        print(f'Analyzing {path}...')
+        with open(path, encoding='utf-8') as f:
+            string = 0
+            for line in f:
+                string += 1
+                if not search_text in line:
+                    continue
+                text.insert(END, f'File: {path[abspath_len:]}\nLine: {string} ')
+                text.window_create(END, window=copy_button('Copy line', line[:-1]))
+                text.window_create(END, window=open_button(path))
+                text.insert(END, f'\n{line}\n')
+                yield
 
-def analyze(event=None):
-    search = entry.get()
+sg = range(0)
+
+def start_search(event=None):
+    global sg
+    sg = find_string_gen(entry.get())
+    continue_search()
+
+def continue_search(event=None, count=50):
+    global sg
     text.delete(1.0, END)
-    counter = 0
-    for FILE in files_list:
-        print(f'Analyzing {FILE}')
-        f = open(FILE, encoding='utf-8')
-        string = 1
-        for line in f:
-            if not search in line:
-                continue
-            counter += 1
+    for i in range(count):
+        try:
+            next(sg)
+        except StopIteration:
+            label['text'] = f'{i} new items are found'
+            break
+    else:
+        label['text'] = f'{count} new items are found'
 
-            text.insert(END, f'File: {FILE[abspath_len:]} ')
-            text.window_create(END, window=show_path_button(FILE))
-            text.window_create(END, window=open_button(FILE))
-            text.insert(END, f'\nLine: {string} ')
-            text.window_create(END, window=copy_button('Copy line', line))
-            text.insert(END, f'\n{line}\n')
-            string += 1
-        f.close()
-    label['text'] = f"{counter} items are found"
+def full_search(event=None):
+    global sg
+    sg = find_string_gen(entry.get())
+    text.delete(1.0, END)
+    label['text'] = f'{len(tuple(sg))} items are found'
 
 
 files_list = []
@@ -69,24 +75,34 @@ for branch in tree:
             if path[-3:] == '.py':
                 files_list.append(os.path.join(branch[0], path))
 
-bg = '#002240'
-fg = '#eeeeee'
-font = 'Consolas 11'
+fg = '#ffddbf'
+vdict = {'bg': '#002240', 'fg': fg, 'font': 'Consolas 11'}
+bdict = {'bg': '#002240', 'fg': '#b1ffeb', 'font': 'Consolas 11'}
 
 root = Tk()
 root.configure(bg='')
 root.title('Code analyzer')
 
-entry = Entry(root, bg=bg, fg=fg, insertbackground=fg, justify="center", font=font)
-button = Button(root, text='Анализировать', command=analyze, bg=bg, fg=fg, font=font)
-label = Label(root, bg=bg, fg=fg, font=font)
-text = Text(wrap=WORD, bg=bg, fg=fg, insertbackground=fg, font=font)
-scrollbar = Scrollbar(command=text.yview, bg=bg, troughcolor=fg)
+entry = Entry(root, **vdict, insertbackground=fg, justify="center")
 
-entry.bind('<Return>', analyze)
+frame = Frame(root)
+button1 = Button(frame, text='Find', command=start_search, **vdict)
+button2 = Button(frame, text='Find more', command=continue_search, **vdict)
+button3 = Button(frame, text='Find all', command=full_search, **vdict)
+
+label = Label(root, **vdict)
+text = Text(wrap=WORD, **vdict, insertbackground=fg)
+scrollbar = Scrollbar(command=text.yview)
+
+entry.bind('<Return>', full_search)
 
 entry.pack(fill=X)
-button.pack(fill=X)
+
+button3.pack(side=RIGHT)
+button2.pack(side=RIGHT)
+button1.pack(fill=X)
+frame.pack(fill=X)
+
 label.pack(fill=X)
 text.pack(side=LEFT, fill=BOTH, expand=1)
 scrollbar.pack(side=RIGHT, fill=Y)
