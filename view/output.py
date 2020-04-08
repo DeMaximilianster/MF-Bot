113e8f5377542a5b2a52c3f676fa144be1e24501
DeMax <trollbol213@gmail.com>
2020-04-08 10:54:40 +0300

    fix: wrapped all output.py functions into try-except for correct returning. gitignore removed one space

--- view/output.py
+++ view/output.py
@@ -1,5 +1,6 @@
 """Module with functions to send to Telegram servers"""
 
+from telebot.apihelper import ApiException
 from presenter.config.token import BOT
 import presenter.config.log as log
 
@@ -11,10 +12,12 @@ def send(chat_id, message_text, **kwargs):
     :arg chat_id
     :arg message_text
     :key parse_mode
-    :key reply_markup
-    :key disable_web_page_preview"""
+    :key reply_markup"""
     LOG.log_print("send invoked")
-    return BOT.send_message(chat_id, message_text, **kwargs)
+    try:
+        return BOT.send_message(chat_id, message_text, disable_web_page_preview=True, **kwargs)
+    except ApiException as exception:
+        LOG.log_print(exception)
 
 
 def send_photo(chat_id, photo, **kwargs):
@@ -26,7 +29,10 @@ def send_photo(chat_id, photo, **kwargs):
     :key reply_markup
     :key parse_mode"""
     LOG.log_print("send_photo invoked")
-    return BOT.send_photo(chat_id, photo, **kwargs)
+    try:
+        return BOT.send_photo(chat_id, photo, **kwargs)
+    except ApiException as exception:
+        LOG.log_print(exception)
 
 
 def send_video(chat_id, video, **kwargs):
@@ -38,13 +44,19 @@ def send_video(chat_id, video, **kwargs):
     :key reply_markup
     :key parse_mode"""
     LOG.log_print("send_video invoked")
-    return BOT.send_video(chat_id, video, **kwargs)
+    try:
+        return BOT.send_video(chat_id, video, **kwargs)
+    except ApiException as exception:
+        LOG.log_print(exception)
 
 
 def send_sticker(chat_id, sticker, reply_to_message_id=None):
     """Send a sticker"""
     LOG.log_print("send_sticker invoked")
-    return BOT.send_sticker(chat_id, sticker, reply_to_message_id=reply_to_message_id)
+    try:
+        return BOT.send_sticker(chat_id, sticker, reply_to_message_id=reply_to_message_id)
+    except ApiException as exception:
+        LOG.log_print(exception)
 
 
 def send_document(chat_id, data, **kwargs):
@@ -55,7 +67,10 @@ def send_document(chat_id, data, **kwargs):
     :key caption
     :key parse_mode"""
     LOG.log_print("send_document invoked")
-    return BOT.send_document(chat_id, data, **kwargs)
+    try:
+        return BOT.send_document(chat_id, data, **kwargs)
+    except ApiException as exception:
+        LOG.log_print(exception)
 
 
 def reply(message, message_text, **kwargs):
@@ -63,22 +78,30 @@ def reply(message, message_text, **kwargs):
     :arg message: message bot replies to
     :arg message_text
     :key parse_mode
-    :key reply_markup
-    :key disable_web_page_preview"""
+    :key reply_markup"""
     LOG.log_print("reply invoked")
-    return BOT.reply_to(message, message_text, **kwargs)
+    try:
+        return BOT.reply_to(message, message_text, disable_web_page_preview=True, **kwargs)
+    except ApiException as exception:
+        LOG.log_print(exception)
 
 
 def forward(chat_id, from_chat_id, message_id):
     """Forward a message"""
     LOG.log_print("forward invoked")
-    return BOT.forward_message(chat_id, from_chat_id, message_id)
+    try:
+        return BOT.forward_message(chat_id, from_chat_id, message_id)
+    except ApiException as exception:
+        LOG.log_print(exception)
 
 
 def edit_markup(chat_id, message_id, reply_markup=None):
     """Edit buttons of the message"""
     LOG.log_print("edit_markup invoked")
-    return BOT.edit_message_reply_markup(chat_id, message_id, reply_markup=reply_markup)
+    try:
+        return BOT.edit_message_reply_markup(chat_id, message_id, reply_markup=reply_markup)
+    except ApiException as exception:
+        LOG.log_print(exception)
 
 
 def edit_text(text, chat_id, message_id, **kwargs):
@@ -87,22 +110,31 @@ def edit_text(text, chat_id, message_id, **kwargs):
     :arg chat_id
     :arg message_id
     :key parse_mode
-    :key reply_markup
-    :key disable_web_page_preview"""
+    :key reply_markup"""
     LOG.log_print("edit_text invoked")
-    return BOT.edit_message_text(text, chat_id, message_id, **kwargs)
+    try:
+        return BOT.edit_message_text(text, chat_id, message_id,
+                                     disable_web_page_preview=True, **kwargs)
+    except ApiException as exception:
+        LOG.log_print(exception)
 
 
 def delete(chat_id, message_id):
     """Delete a message"""
     LOG.log_print("delete invoked")
-    return BOT.delete_message(chat_id, message_id)
+    try:
+        return BOT.delete_message(chat_id, message_id)
+    except ApiException as exception:
+        LOG.log_print(exception)
 
 
 def kick(chat_id, user_id):
     """Kick a user"""
     LOG.log_print("kick invoked")
-    return BOT.kick_chat_member(chat_id, user_id)
+    try:
+        return BOT.kick_chat_member(chat_id, user_id)
+    except ApiException as exception:
+        LOG.log_print(exception)
 
 
 def restrict(chat_id, user_id, **kwargs):
@@ -115,13 +147,19 @@ def restrict(chat_id, user_id, **kwargs):
     :key can_send_other_messages
     :key can_add_web_page_previews"""
     LOG.log_print("restrict invoked")
-    return BOT.restrict_chat_member(chat_id, user_id, **kwargs)
+    try:
+        return BOT.restrict_chat_member(chat_id, user_id, **kwargs)
+    except ApiException as exception:
+        LOG.log_print(exception)
 
 
 def unban(chat_id, user_id):
     """Unban member"""
     LOG.log_print("unban invoked")
-    return BOT.unban_chat_member(chat_id, user_id)
+    try:
+        return BOT.unban_chat_member(chat_id, user_id)
+    except ApiException as exception:
+        LOG.log_print(exception)
 
 
 def promote(chat_id, user_id, **kwargs):
@@ -137,40 +175,61 @@ def promote(chat_id, user_id, **kwargs):
     :key can_pin_messages
     :key can_promote_members"""
     LOG.log_print(f"promote invoked chat_id={chat_id}, user_id={user_id}")
-    return BOT.promote_chat_member(chat_id, user_id, **kwargs)
+    try:
+        return BOT.promote_chat_member(chat_id, user_id, **kwargs)
+    except ApiException as exception:
+        LOG.log_print(exception)
 
 
 def answer_inline(inline_query_id, results, cache_time=None):
     """Show a result of an inline-request"""
     LOG.log_print("answer_inline invoked")
-    return BOT.answer_inline_query(inline_query_id, results, cache_time=cache_time)
+    try:
+        return BOT.answer_inline_query(inline_query_id, results, cache_time=cache_time)
+    except ApiException as exception:
+        LOG.log_print(exception)
 
 
 def answer_callback(callback_query_id, text=None, show_alert=False):
     """Show a notification on a top of a messsage box (or a alert)"""
     LOG.log_print("answer_callback invoked")
-    return BOT.answer_callback_query(callback_query_id, text, show_alert)
+    try:
+        return BOT.answer_callback_query(callback_query_id, text, show_alert)
+    except ApiException as exception:
+        LOG.log_print(exception)
 
 
 def register_handler(sent, function, *args, **kwargs):
     """Next message will be processed with specified function"""
     LOG.log_print("register_handler invoked")
-    return BOT.register_next_step_handler(sent, function, *args, **kwargs)
+    try:
+        return BOT.register_next_step_handler(sent, function, *args, **kwargs)
+    except ApiException as exception:
+        LOG.log_print(exception)
 
 
 def get_member(chat_id, user_id):
     """Get a member of a chat"""
     LOG.log_print("get_member invoked")
-    return BOT.get_chat_member(chat_id, user_id)
+    try:
+        return BOT.get_chat_member(chat_id, user_id)
+    except ApiException as exception:
+        LOG.log_print(exception)
 
 
 def get_chat(chat_id):
     """Get a chat"""
     LOG.log_print("get_chat invoked")
-    return BOT.get_chat(chat_id)
+    try:
+        return BOT.get_chat(chat_id)
+    except ApiException as exception:
+        LOG.log_print(exception)
 
 
 def get_me():
     """ Get information about bot """
     LOG.log_print("get_me invoked")
-    return BOT.get_me()
+    try:
+        return BOT.get_me()
+    except ApiException as exception:
+        LOG.log_print(exception)
