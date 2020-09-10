f508410550369a76076fae51f45ffa080d41a3b4
apo-diod <podimov.andrey.odessa@gmail.com>
2020-09-10 15:38:50 +0300

    fixed invalid method call

--- presenter/logic/api_requests.py
+++ presenter/logic/api_requests.py
@@ -6,9 +6,9 @@ LOG = Logger(0) # int code for log_to_both
 
 def request_file(file_id, save_as):
 	file_info = BOT.get_file(file_id)
-	file_in = requests.get("https://api.telegram.org/file/bot{0}/{1}".format(BOT.token, file_info.file_path))
-	file_out = open("input+"+file_in.name, "w")
-	lines = file_in.read()
+	req = requests.get("https://api.telegram.org/file/bot{0}/{1}".format(BOT.token, file_info.file_path))
+	file_out = open("input+"+file_info.file_path, "w")
+	lines = req.content()
 	file_out.write(lines)
-	file_in.close()
+	req.close()
 	file_out.close()
